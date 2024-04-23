288d0135f79b74aa10ae5a16ae1a498cc723e909
fpiqueres <fpiqueres@live.com>
2024-04-23 23:42:38 +0200

    Ajuste función de nominalización

--- Facilitada/Sintactico/Nominalizacion.py
+++ Facilitada/Sintactico/Nominalizacion.py
@@ -1,33 +1,24 @@
 import spacy
-import re
 
 class Nominalizacion:
     def __init__(self, modelo='es_core_news_sm'):
         self.nlp = spacy.load(modelo)
-        # Diccionario de excepciones
-        self.excepciones = {'primer': True,
-                            'azúcar': True,
-                            'lugar': True,
-                            'oscar': True,
-                            'óscar': True,
-                            'mujer': True,  #en principio no es necesaria al ser femenimo La mujer no entraría en el código de detección.
-                            'taller': True,
-                            'trailer': True,
-                            'ayer': True,
-                            'mayer': True,
-                            'camper': True,
-                            }
+        self.excepciones = {
+            'primer': True, 'azúcar': True, 'lugar': True, 'oscar': True,
+            'óscar': True, 'mujer': True, 'taller': True, 'trailer': True,
+            'ayer': True, 'mayer': True, 'camper': True,
+        }
 
     def detectar_nominalizacion(self, texto):
         doc = self.nlp(texto)
         posiciones = []
 
         for token in doc:
+            # Comprobar que el token es un verbo o que está precedido por "el"
             if token.text.lower().endswith(('ar', 'er', 'ir')) and not self.excepciones.get(token.text.lower(), False):
                 if token.pos_ == "VERB" or (token.i > 0 and doc[token.i - 1].lower_ == 'el'):
                     inicio = doc[token.i - 1].idx if (token.i > 0 and doc[token.i - 1].lower_ == 'el') else token.idx
-                    fin = inicio + len(doc[token.i - 1].text) + len(token.text) if (
-                        token.i > 0 and doc[token.i - 1].lower_ == 'el') else token.idx + len(token.text)
+                    fin = doc[token.i].idx + len(doc[token.i].text)
                     es_capitalizado = doc[token.i - 1].text[0].isupper() if (
                         token.i > 0 and doc[token.i - 1].lower_ == 'el') else token.text[0].isupper()
                     posiciones.append((inicio, fin, es_capitalizado, token.text))
@@ -39,22 +30,10 @@ class Nominalizacion:
         texto_modificado = texto
 
         for inicio, fin, es_capitalizado, verbo in sorted(posiciones, reverse=True):
-            # Comprobar si hay un punto justo después del verbo y ajustar `fin` si es necesario
-            preservar_punto = texto[fin] == '.' if fin < len(texto) else False
-            fin_ajustado = fin + 1 if preservar_punto else fin
-
-            # Extraer el verbo correctamente y aplicar las transformaciones necesarias
-            verbo_extraido = texto[inicio:fin].strip()
-            if es_capitalizado:
-                verbo_extraido = verbo_extraido.capitalize()
-
-            # Aplicar re.sub para asegurar que no duplicamos 'r'
-            verbo_extraido = re.sub(r'rr$', 'r', verbo_extraido)
-
-            reemplazo = verbo_extraido + ('.' if preservar_punto else '')
+            verbo_extraido = verbo.capitalize() if es_capitalizado else verbo
 
             # Realizar el reemplazo en el texto
-            texto_modificado = texto_modificado[:inicio] + reemplazo + texto_modificado[fin_ajustado:]
+            texto_modificado = texto_modificado[:inicio] + verbo_extraido + texto_modificado[fin:]
 
         return texto_modificado
 
@@ -66,12 +45,11 @@ class Nominalizacion:
         for token in doc:
             if token.text.lower().endswith(('ar', 'er', 'ir')) and token.pos_ == "VERB":
                 if token.i > 0 and doc[token.i - 1].lower_ == 'el':
-                    nominalizaciones.append(f'{doc[token.i-1].text} {token.text}')
+                    nominalizaciones.append(f'{doc[token.i - 1].text} {token.text}')
             elif token.lower_ == 'se' and token.head.pos_ == 'VERB' and token.head.i > token.i:
                 impersonales.append(f'{token.text} {token.head.text}')
 
         return {'nominalizaciones': nominalizaciones, 'impersonales': impersonales}
-
 
     def detectar_nominalizacion_solo_frases(self, texto):
         doc = self.nlp(texto)
@@ -84,3 +62,4 @@ class Nominalizacion:
                     nominalizaciones.append(nominalizacion)
 
         return nominalizaciones
+
